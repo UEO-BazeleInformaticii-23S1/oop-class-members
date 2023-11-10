72e32393e4c858193d92c0ca0639d1bb8e4321d5
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-10 09:25:48 +0200

    examples with readonly fields

--- OopClassMembers/Program.cs
+++ OopClassMembers/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int maxLifespan = Person.MaxLifespanYears;
+            Person p = new Person("2344");
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.CNP);
+
+            //Person.MaxLifespanYears
+            //p.CNP = "2344";
+            Person.PopulationCount = 1;
         }
     }
 }
