4459c0e7e4eee2d52bb26e478ec4a5a23f45778f
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-13 09:15:35 +0200

    read a list of persons from keyboard

--- OopClassMembers/Person.cs
+++ OopClassMembers/Person.cs
@@ -2,63 +2,28 @@
 {
     internal class Person
     {
-        private string _firstName = "John";
-
-        private string _lastName = "Doe";
-
-        // readonly field can be modified from the initialization expression (#1)
-        private readonly string _cnp = "1234";
-
-        private static int _populationCount = 0;
-
         public static readonly int MaxLifespanYears = InitMaxLifespan();
 
         // this is a constructor
         public Person(string cnp)
         {
             // readonly field can be modified from the constructor (#2)
-            _cnp = cnp;
+            Cnp = cnp;
 
             // each time a new person gets created, populationCount is incremented
-            Person._populationCount++;
+            Person.PopulationCount++;
         }
 
-        public string FirstName
-        {
-            get 
-            {
-                return _firstName;
-            }
-            set 
-            { 
-                if (!string.IsNullOrEmpty(value))
-                {
-                    _firstName = value;
-                }
-                else
-                {
-                    // or raise error
-                    _firstName = string.Empty;
-                }
-                
-            }
-        }
+        public string FirstName { get; set; } = "";
 
-        public string LastName
-        {
-            get { return _lastName; }
-            set { _lastName = value; }
-        }
+        public string LastName { get; set; } = "";
 
         public string Cnp
         {
-            get { return _cnp; }
+            get;
         }
 
-        public static int PopulationCount
-        {
-            get { return Person._populationCount; }
-        }
+        public static int PopulationCount { get; private set; } = 0;
 
         public void PrintPerson()
         {
--- OopClassMembers/Program.cs
+++ OopClassMembers/Program.cs
@@ -4,19 +4,67 @@
     {
         static void Main(string[] args)
         {
-            int maxLifespan = Person.MaxLifespanYears;
-            Person p = new Person("2344");
-            p.FirstName = "Florin";
+            int noOfPersons = ReadNumber("Number of persons=", 3);
+            Person[] arrayOfPersons = new Person[noOfPersons];
+            for (int i = 0; i < noOfPersons; i++)
+            {
+                Console.Write("FirstName=");
+                string firstName = Console.ReadLine();
 
-            Console.WriteLine(Person.PopulationCount);
-            Console.WriteLine(p.FirstName);
-            Console.WriteLine(p.LastName);
-            Console.WriteLine(p.Cnp);
+                Console.Write("LastName=");
+                string lastName = Console.ReadLine();
+
+                Console.Write("CNP=");
+                string cnp = Console.ReadLine();
+
+                Person p = new Person(cnp);
+                p.FirstName = firstName;
+                p.LastName = lastName;
+
+                arrayOfPersons[i] = p;
+            }
+
+
+            //int maxLifespan = Person.MaxLifespanYears;
+            //Person p = new Person("2344");
+            //p.FirstName = "Florin";
+
+            //Console.WriteLine(Person.PopulationCount);
+            //Console.WriteLine(p.FirstName);
+            //Console.WriteLine(p.LastName);
+            //Console.WriteLine(p.Cnp);
 
 
             //Person.MaxLifespanYears
-            //p.CNP = "2344";
+            //p.Cnp = "2344";
             //Person.PopulationCount = 1;
+        }
+
+        static int ReadNumber(string label, int maxAttempts)
+        {
+            int attempts = 1;
+            do
+            {
+                Console.Write(label);
+                string input = Console.ReadLine();
+                bool isValidNumber = int.TryParse(input, out int result);
+
+                if (isValidNumber)
+                {
+                    return result;
+                }
+
+                if (attempts < maxAttempts)
+                {
+                    Console.WriteLine($"Value '{input}' is not a valid number, please try again.");
+                }
+
+                attempts++;
+            }
+            while (attempts <= maxAttempts);
+
+            Console.WriteLine($"No valid numeric input, continuing with 0 as default value.");
+            return 0;
         }
     }
 }
